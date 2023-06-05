0b5ca0017c3e62c570eda5b1ba8738c071566203
Logon Aluno <logonrmlocal@fiap.com.br>
2023-06-05 12:23:10 -0300

    adicionado exemplos forms

---
+++ exemplosForms/oi.js
@@ -0,0 +1,12 @@
+console.log("nome\tnota\tturma")
+console.log("pedro\t10\t2mic")
+console.log("paulo\t09\t2mic")
+console.log("carla\t10\t2mic")
+
+let nome = "peDro"
+let valor1 = 10
+let valor2 = 20
+let valor3 = valor1 + valor2
+
+console.log("oi meu amigo(a)",nome.toLowerCase())
+console.log("as somas dos valores é :",valor3)
